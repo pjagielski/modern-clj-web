58df4a79c765f077f379449d980999f96fc3541a
Piotr Jagielski <pjagielski@o2.pl>
2015-09-20 22:41:34 +0200

    not needed lifecycle in repository

--- src/modern_clj_web/component/repo.clj
+++ src/modern_clj_web/component/repo.clj
@@ -1,16 +1,11 @@
 (ns modern-clj-web.component.repo
-  (:require [com.stuartsierra.component :as component]
-            [monger.collection :as mc]
+  (:require [monger.collection :as mc]
             [monger.json]))
 
 (defprotocol ContactRepository
   (find-all [this]))
 
 (defrecord ContactComponent [mongo]
-  component/Lifecycle
-  (start [this] this)
-  (stop [this] this)
-
   ContactRepository
   (find-all [this]
     (mc/find-maps (:db mongo) "contacts")))
